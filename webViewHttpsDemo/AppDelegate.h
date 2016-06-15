9db887a6895180b2dcb25895d9bdb3c01b5174f0
SongZhen <591555331@qq.com>
2016-06-15 11:08:42 +0800

    Initial Commit

---
+++ webViewHttpsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  webViewHttpsDemo
+//
+//  Created by 又土又木 on 16/6/15.
+//  Copyright © 2016年 ytuymu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
